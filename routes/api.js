70e3a605d1e334e8bdb501de6cc8f80a07a57870
wei0127 <d222015337@gmail.com>
2023-05-16 17:11:42 +0800

    first

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const sqlite = require('sqlite3').verbose();
+db = new sqlite.Database("./db.sqlite", sqlite.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+router.post('/', (req, res) => {
+    const {name, day, price}=req.body;
+    sql = "INSERT INTO quote (name, day, price) VALUES (?, ?, ?)";
+    db.run(sql, [name, day, price], (err) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).send(err.message);
+        }
+        console.log('inserted');
+    });
+    //res.redirect('/data.html');
+    return res.status(200).send('inserted');
+})
+
+router.get('/', function(req, res, next) {
+    sql= "SELECT * FROM quote";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.send(rows);
+    });
+});
+
+module.exports = router;
